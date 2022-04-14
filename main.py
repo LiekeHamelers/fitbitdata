729445f9e7863c5daa7ecdc07ec86808ec5bda04
Lieke <lieke.hamelers@gmail.com>
2022-04-14 21:14:31 +0200

    read data

---
+++ main.py
@@ -0,0 +1,26 @@
+#%% import packages and set variables
+import pandas as pd
+import numpy as np
+import glob
+import os
+
+variable = 'distance'
+jsonpath = "C:/Users/lieke/OneDrive/Documents/Programmeren/fitbitdata/LiekeHamelers/Physical Activity/"
+
+#%% - Read data
+file_list = glob.glob(jsonpath + variable + '*.json')
+
+#%%
+df = pd.DataFrame()
+for file in file_list:
+    ent = pd.read_json(file)
+    print(ent)
+    if df.empty: 
+        df = ent
+    else: 
+        print("Here")
+        df = pd.concat([df, ent], axis = 0)
+        print(len(df))
+
+
+# %%
